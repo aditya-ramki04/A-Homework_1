123a960f667d1f10fdf93f2a3512146905f529fd
Nitin Chava <nitinchava10@gmail.com>
2024-01-31 17:08:47 -0600

    hopefully this commit works

--- src/hello/Homework_1.java
+++ src/hello/Homework_1.java
@@ -6,8 +6,8 @@ public class Homework_1 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Abhi Pasupuleti. GitHub ID: #158082744");
-		System.out.println("Aditya Ramakrishnan. Github ID: 112342054");
-		System.out.println("Nitin M. Chava. Github Username: nmc-cs");	
+		System.out.println("Aditya Ramakrishnan. Github ID: #112342054");
+		System.out.println("Nitin M. Chava. Github ID: #113474410");	
 		System.out.println("Matthew Robison. GitHub ID: #115325382");
 		
 	}
