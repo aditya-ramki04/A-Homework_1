88667d5b096c55646d022e3efa2b62e2343098a2
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2024-01-31 14:42:50 -0600

    fixed github id

--- src/hello/Homework_1.java
+++ src/hello/Homework_1.java
@@ -6,7 +6,7 @@ public class Homework_1 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Abhi Pasupuleti. GitHub ID: #158082744");
-		System.out.println("Aditya Ramakrishnan. Github Username: aditya-ramki04");
+		System.out.println("Aditya Ramakrishnan. Github ID: 112342054");
 		System.out.println("Nitin M. Chava. Github Username: nmc-cs");	
 		System.out.println("Matthew Robison. Github Username: mattrobi04");
 		
